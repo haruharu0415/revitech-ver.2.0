4c1922b9111fe2146a73be373daaa6bf594c979b
unknown <kudou.haruru@gmail.com>
2025-09-29 18:13:35 +0900

    harukunkakusei

--- src/main/java/com/example/revitech/entity/Teacher.java
+++ src/main/java/com/example/revitech/entity/Teacher.java
@@ -1,5 +1,41 @@
 package com.example.revitech.entity;
 
-public class Teacher {
+import java.time.LocalDate;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "home")
+@Data
+@NoArgsConstructor
+public class Teacher {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	private Integer id;
+	
+	private String name;
+	
+	private String email;
+	
+	private String password;
+	
+	private String status;
+	
+	private String role;
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate create_at;
+	
+	private LocalDate update_at;
+	
+	
 }
