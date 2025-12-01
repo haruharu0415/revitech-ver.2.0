a64ed0dcb4111e06422d760baa8fd1de08361de4
unknown <24yn0104@jec.ac.jp>
2025-12-01 19:50:42 +0900

    aaaaaaaaa

---
+++ src/main/java/com/example/revitech/entity/Teacher.java
@@ -0,0 +1,48 @@
+package com.example.revitech.entity;
+
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "home")
+@Data
+@NoArgsConstructor
+public class Teacher {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	private Integer id;
+	
+	private String name;
+	
+	private String email;
+	
+	private String password;
+	
+	private String status;
+	
+	private String role;
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate create_at;
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate udpate_at;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "id",insertable=false ,updatable = false)
+	Teacher teachers;
+}
