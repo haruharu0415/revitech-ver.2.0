77b4c5153345cf4ba893b2f838b5c9f8a0b0fde1
unknown <24yn0129@jec.ac.jp>
2025-12-02 19:05:18 +0900

    yuta

--- src/main/java/com/example/revitech/RevitechApplication.java
+++ src/main/java/com/example/revitech/RevitechApplication.java
@@ -15,6 +15,8 @@ public class RevitechApplication {
     public static void main(String[] args) {
         SpringApplication.run(RevitechApplication.class, args);
     }
+    
+    //aaaa
 
     /**
      * ★★★ このBeanをファイルの一番下に追加してください ★★★
